56447474b93279d7a6fec7ae185874144f40ed3a
Matt Lantin <mlantin1093@gmail.com>
2024-06-27 22:40:38 -0400

    GitIgnore Firebase and Modified App.jsx

---
+++ netflix-clone/src/components/HomeScreen.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from 'styled-components'
+
+function HomeScreen() {
+    return (
+        <mainDiv>
+            
+        </mainDiv>
+    )
+}
+
+export default HomeScreen;
+
+
+
+
+const mainDiv = styled.div 
+`
+    
+`
